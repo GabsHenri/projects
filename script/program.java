d1a7c391f08765a2cedcbfc4c81291f311aec440
recepcao <moneycrashing76@gmail.com>
2023-11-22 09:40:23 -0300

    Programa para alocar números em um vetor, exibir os números alocados, somar e tirar média dos números - Exercicio treino de uso dos vetores

--- script/program.java
+++ script/program.java
@@ -10,16 +10,30 @@ public class program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        Product product = new Product();
+        System.out.print("Digite o tamanho do vetor: ");
+        int n = sc.nextInt();
+        Product[] vect = new Product[n];
 
-        System.out.print("Digite o valor da nota fiscal: ");
-        product.a = sc.nextDouble();
-        System.out.print("Digite o valor da retenção da NFSE: ");
-        product.b = sc.nextDouble();
-        System.out.printf("O valor final da NFSE é: %.2f%n", product.calculator());
-        System.out.println("-----------------------------------");
+        for (int i = 0; i < n; i++) {
+            System.out.print("Digite um número: ");
+            double number = sc.nextDouble();
+            vect[i] = new Product(number);
+        }
+
         sc.nextLine();
-        product.condicao();
+        System.out.print("Valores: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(vect[i]);
+        }
+
+        double soma = 0.0;
+        sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            soma += vect[i].getNumber();
+        }
+        System.out.printf("Soma: %.2f%n", soma);
+        double media = soma / n;
+        System.out.printf("Média: %.2f%n", media);
         sc.close();
     }
 }
